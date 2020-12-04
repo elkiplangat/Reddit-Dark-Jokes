5a9ead3da71d6167800a8c9b9ab5c38aaa90c69e
kiplangat <langatkip23@yahoo.com>
2020-12-04 21:54:29 +0300

    add requirements.txt

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import praw
+from decouple import config
 
-client_id = ""
-client_secret = ""
-user_agent = ""
+client_id = config('client_id')
+client_secret = config('client_secret')
+user_agent = config('user_agent')
+
+reddit = praw.Reddit(client_id=client_id, client_secret=client_secret, user_agent=user_agent)
+praw==7.1.0
